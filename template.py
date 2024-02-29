b93513dc1c815d1329eb015b641e6e7955683098
Noah <noah.t.chen@gmail.com>
2024-02-28 22:13:49 -0500

    Update template.py

--- template.py
+++ template.py
@@ -9,24 +9,25 @@ with open(file_path, newline='') as rsrd:
     reader = csv.reader(rsrd)
     rsd = list(reader)
 
-
 # OOP
 names = rsd.pop(0)
 class team:
-    def __init__(self, score=0, mpa=0, mph=0, tg=0, tgaa=0, tgah=0, tp=0, txg=0, mp=0, shots=0, tgc=0, shotsc=0):
-        self.score = score
-        self.mpa = mpa
-        self.mph = mph
-        self.tg = tg
-        self.tgaa = tgaa
+    def __init__(self, score=0, mpa=0, mph=0, tg=0, tgaa=0, tgah=0, tp=0, txg=0, mp=0, shots=0, tgc=0, shotsc=0, corners=0):
+        self.score = score 
+        self.mpa = mpa # match played away
+        self.mph = mph # match played home
+        self.tg = tg # total goals
+        self.tgaa = tgaa 
         self.tgah = tgah
-        self.tp = tp
-        self.txg = txg
-        self.mp = mp
-        self.shots = shots
-        self.shotsc = shotsc
-        self.tgc = tgc
-        self.r = [0,0,0]
+        self.tp = tp # total possession
+        self.txg = txg # total xG
+        self.mp = mp # match played
+        self.shots = shots # total shots
+        self.shotsc = shotsc # total shots conceded
+        self.tgc = tgc # total goals conceded
+        self.r = [0,0,0] # WTL
+        self.corners = corners
+
 rspd = defaultdict(team)
 for r in rsd:
     n,game_id,HomeTeam,AwayTeam,HomeScore,AwayScore,Home_xG,Away_xG,Home_shots,Away_shots,Home_corner,Away_corner,Home_PK_Goal,Away_PK_Goal,Home_PK_shots,Away_PK_shots,Home_ToP = r[:-4]
@@ -48,6 +49,8 @@ for r in rsd:
         rspd[HomeTeam].r[1] += 1
         rspd[AwayTeam].r[1] += 1
     rspd[HomeTeam].mph += 1
+    rspd[HomeTeam].corners += int(Home_corner)
+    rspd[AwayTeam].corners += int(Away_corner)
     rspd[AwayTeam].mpa += 1
     rspd[HomeTeam].tg += int(HomeScore)
     rspd[AwayTeam].tg += int(AwayScore)
@@ -72,38 +75,20 @@ totalGoalsAgainst = [(rspd[i].tgaa+rspd[i].tgah) for i in rsppd]
 avgPosession = [rspd[i].tp/rspd[i].mp for i in rsppd]
 avgxG = [rspd[i].txg/rspd[i].mp for i in rsppd]
 STGconversion = [rspd[i].tg/rspd[i].shots for i in rsppd]
+shotQuality = [rspd[i].txg/rspd[i].shots for i in rsppd]
+efficiency = [rspd[i].txg/rspd[i].tg for i in rsppd]
+qualityP = [(rspd[i].corners+rspd[i].shots)/rspd[i].tp for i in rsppd]
+defQuality = [rspd[i].tgc/rspd[i].shotsc for i in rsppd]
 
-"Total Goals"
+# All of the graphs are ranked by score.
 
-# plt.bar(rsppd, totalGoals, label='Total Goals')
+#----Standings----
 
-"Total Goals Against"
-
-# plt.plot(totalGoalsAgainst, label='Average Goals Against')
-
-"Total Score (3 * Wins + Ties)"
+"1. Total Score (3 * Wins + Ties)"
 
 # plt.bar(rsppd, score, label="Score")
 
-"Average Time of Possession"
-
-# plt.bar(rsppd, avgPosession, label='Average Time of Possession')
-
-"Shots to Goals conversion"
-
-# plt.bar(rsppd, STGconversion, label='Shot to goals conversion')
-
-"Average xG"
-
-# plt.plot(avgxG, label='Average xG')
-
-"Rating System (not finished)"
-
-# elo = [(rspd[i].tg-rspd[i].tga+rspd[i].txg)/rspd[i].mp for i in rsppd]
-# plt.plot(score, label='score')
-# plt.plot(elo, label='elo')
-
-"Team WTL"
+"2. Team WTL"
 
 # barWidth = 0.2
 # br1 = np.arange(len(rsppd)) 
@@ -120,7 +105,56 @@ STGconversion = [rspd[i].tg/rspd[i].shots for i in rsppd]
 # plt.bar(br4, L, color ='r', width = barWidth, edgecolor ='grey', label ='L') 
 # plt.xticks([r+barWidth for r in range(len(rsppd))], rsppd)
 
-"Labels"
+#----Offense----
+
+"3. Shot Quality"
+
+# plt.bar(rsppd, shotQuality, label='Shot Quality')
+ 
+"4. Efficiency"
+
+# plt.bar(rsppd, efficiency, label='Efficiency')
+
+"5. Shots to Goals conversion"
+
+# plt.bar(rsppd, STGconversion, label='Shot to goals conversion')
+
+"6. Quality of Possession"
+
+# plt.bar(rsppd, qualityP, label='Quality of Possession')
+
+"7. Total Goals For"
+
+# plt.bar(rsppd, totalGoals, label='Total Goals')
+
+"8. Average Time of Possession"
+
+# plt.bar(rsppd, avgPosession, label='Average Time of Possession')
+
+"-. Average xG"
+
+# plt.plot(avgxG, label='Average xG')
+
+#----Defense----
+
+"9. Defense Efficiency"
+
+# plt.bar(rsppd, defQuality, label='Defensive Quality')
+
+"10. Total Goals Against"
+
+plt.bar(rsppd, totalGoalsAgainst, label='Total Goals Against')
+
+# ----Rankings----
+
+"-. Rating System (not finished)"
+
+# elo = [(rspd[i].tg-rspd[i].tga+rspd[i].txg)/rspd[i].mp for i in rsppd]
+# plt.plot(score, label='score')
+# plt.plot(elo, label='elo')
+
+10101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010
+# Labels
 
 plt.xlabel('Rank')  
 plt.ylabel('Values')
